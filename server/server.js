6bf533745eb7f0be14eb20b32b907438fe76ea9d
shubham-95 <shubhamsharma032@gmail.com>
2018-11-25 22:31:20 +0530

    added get todos endpoints

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +29,22 @@ app.get('/getTodos',(req,res)=>{
    }).catch((err)=>{
        res.status(400).send(err);
    }) ;
+});
+
+app.get('/todos/:id',(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    Todo.findById(req.params.id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();    
+        }
+        res.status(200).send(todo);
+        
+    })
+    .catch((err)=>{
+        res.status(404).send();
+    });
 });
 
 app.listen(3000,(res)=>{
